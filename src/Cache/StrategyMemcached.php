03f1fd6aacf35aabc9e2c91a95df45784e0b1cb3
akoryak <akoryak@gmail.com>
2025-07-19 20:30:16 -0400

    feature/cache2: php 7.4 compatibility

--- src/Cache/StrategyMemcached.php
+++ src/Cache/StrategyMemcached.php
@@ -4,10 +4,11 @@ namespace Akoryak\Components\Cache;
 
 use Memcached;
 
-class StrategyMemcached extends Memcached implements StrategyInterface {
+class StrategyMemcached extends Memcached {
 	
 	public function connect(string $host, string $port) : bool {
 		// Memcached::OPT_COMPRESSION is set by defaulf for all values longer then XXX bytes, where XXX depends on PHP version
 		return $this->addServer($host, $port);
 	}
+
 }
